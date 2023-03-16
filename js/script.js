45c72c0a3947cda622bff4a1256b0cb7728850f0
anashamimi <anashamimi2@gmail.com>
2023-03-16 14:46:32 +0100

    end exercise palidroma

--- js/script.js
+++ js/script.js
@@ -9,3 +9,41 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 */
+
+
+const wordBox = document.getElementById('word');
+const btnSend = document.querySelector('button');
+
+btnSend.addEventListener('click', palindromo);
+
+//funzione per cercare sela parola è palindroma
+function palindromo() {
+    const word = wordBox.value.split(" ").join("");
+    const wordSplit = word.split("");
+    const reverseWord = wordSplit.reverse().join("");
+    let message = '';
+    let input = '';
+
+    console.log(word);
+    console.log(reverseWord);
+
+    //condizione che verifica se l'utente ha inserito una parola o una frase
+    if (wordBox.value.includes(" ")) {
+        input = 'frase';
+        console.log("La stringa contiene degli spazi");
+    } else {
+        input = 'parola';
+        console.log("La stringa non contiene degli spazi");
+    }
+
+    //condizione che verifica se parola/frase è palidroma
+    if (word === reverseWord) {
+        message = `${wordBox.value} è una ${input} palidroma`;
+        console.log(true);
+    } else {
+        message = `${wordBox.value} non è una ${input} palidroma`;
+        console.log(false);
+    }
+
+    console.log(message)
+}
